a4ab9ec0265847bbfaeea303285f6c7a25039ebb
pedrotfs <pedrotfs@gmail.com>
2020-10-05 17:00:27 -0300

    v01

---
+++ resources/js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(() => {
+    /**test */
+    $(".logo").click(() => {
+        $("*").css("color", "#cc1111")
+    })
+
+    /* sticky navigation */
+    $(".js--section-features").waypoint((direction) => {
+        if(direction == "down") {
+            $("nav").addClass("sticky")
+        } else {
+            $("nav").removeClass("sticky")
+        }
+    }, {
+        offset:"60px;"
+    })
+    
+    $(".js--nav-icon").click(() => {
+        var nav = $(".js-main-navigation")
+        nav.slideToggle(200)
+        var icon = $(".js--nav-icon i")
+        if(icon.hasClass("ion-navicon-round")) {
+            icon.addClass("ion-close-round")
+            icon.removeClass("ion-navicon-round")
+        } else {
+            icon.addClass("ion-navicon-round")
+            icon.removeClass("ion-close-round")
+        }
+    })
+
+
+})
